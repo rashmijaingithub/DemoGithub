e4856090381d13380fa5132b4838fbc049052841
rashmijaingithub <rash.jain29@gmail.com>
2019-07-13 22:16:03 +0530

    first commit

---
+++ src/main/java/com/qa/demo/github/DemoGitHub.java
@@ -0,0 +1,14 @@
+package com.qa.demo.github;
+
+import org.testng.annotations.Test;
+
+public class DemoGitHub {
+	
+	@Test
+	public void demo()
+	{
+		System.out.println("Tetsing git hub");
+		System.out.println("Commit");
+	}
+
+}
